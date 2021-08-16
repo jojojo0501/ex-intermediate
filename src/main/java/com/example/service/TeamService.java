6bf139b283db000435a3c50d3359b507edb52b34
jojojo0501 <sarukiti0418@outlook.jp>
2021-08-16 18:01:57 +0900

    Teamサービスクラスの作成、showListメソッドの実装

--- src/main/java/com/example/service/TeamService.java
+++ src/main/java/com/example/service/TeamService.java
@@ -1,10 +1,31 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Team;
+import com.example.repository.TeamRepository;
+
 /**
  * チーム情報を操作するサービス.
  * @author kanekojota
  *
  */
+@Service
+@Transactional
 public class TeamService {
 
+	@Autowired
+	private TeamRepository teamRepository;
+	
+	/**
+	 *チーム情報を全件取得する.
+	 * @return 全チームの情報
+	 */
+	public List<Team> showList(){
+		return teamRepository.findAll();
+	}
 }
